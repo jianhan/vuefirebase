82723de214409393ae7bb273abc7b70d45311d58
Jian Han <jianhan@mail.com>
2018-03-21 20:17:38 +1000

    [DEV] Dashboard component

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import LayoutsAuth from '@/layouts/Auth'
 import PagesLogin from '@/pages/Login'
+import PagesDashboard from '@/pages/Dashboard'
+import LayoutsAdmin from '@/layouts/Admin'
 
 Vue.use(Router)
 
@@ -16,6 +18,17 @@ export default new Router({
         {
           path: 'login',
           component: PagesLogin
+        }
+      ]
+    },
+    {
+      path: '/admin',
+      name: 'LayoutsAdmin',
+      component: LayoutsAdmin,
+      children: [
+        {
+          path: 'dashboard',
+          component: PagesDashboard
         }
       ]
     }
