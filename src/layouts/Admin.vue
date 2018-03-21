a73240f57d23dc8419ed10e3ff19d1a542f9e75f
Jian Han <jianhan@mail.com>
2018-03-21 20:40:10 +1000

    [REFACTOR] Added new theme, logout btn

--- src/layouts/Admin.vue
+++ src/layouts/Admin.vue
@@ -1,110 +1,197 @@
 <template>
 <div>
-  <v-navigation-drawer fixed clipped class="grey lighten-4" app v-model="drawer">
-    <v-list dense class="grey lighten-4">
-      <template v-for="(item, i) in items">
-        <v-layout
-          row
-          v-if="item.heading"
-          align-center
-          :key="i"
-        >
-          <v-flex xs6>
-            <v-subheader v-if="item.heading">
-              {{ item.heading }}
-            </v-subheader>
-          </v-flex>
-          <v-flex xs6 class="text-xs-right">
-            <v-btn small flat>edit</v-btn>
-          </v-flex>
-        </v-layout>
-        <v-divider
-          dark
-          v-else-if="item.divider"
-          class="my-3"
-          :key="i"
-        ></v-divider>
-        <v-list-tile
-          :key="i"
-          v-else
-        >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title class="grey--text">
-              {{ item.text }}
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </template>
+  <v-navigation-drawer fixed :clipped="$vuetify.breakpoint.lgAndUp" app v-model="drawer">
+    <v-list dense>
+      <template v-for="item in items">
+       <v-layout
+         row
+         v-if="item.heading"
+         align-center
+         :key="item.heading"
+       >
+         <v-flex xs6>
+           <v-subheader v-if="item.heading">
+             {{ item.heading }}
+           </v-subheader>
+         </v-flex>
+         <v-flex xs6 class="text-xs-center">
+           <a href="#!" class="body-2 black--text">EDIT</a>
+         </v-flex>
+       </v-layout>
+       <v-list-group
+         v-else-if="item.children"
+         v-model="item.model"
+         :key="item.text"
+         :prepend-icon="item.model ? item.icon : item['icon-alt']"
+         append-icon=""
+       >
+         <v-list-tile slot="activator">
+           <v-list-tile-content>
+             <v-list-tile-title>
+               {{ item.text }}
+             </v-list-tile-title>
+           </v-list-tile-content>
+         </v-list-tile>
+         <v-list-tile
+           v-for="(child, i) in item.children"
+           :key="i"
+           @click=""
+         >
+           <v-list-tile-action v-if="child.icon">
+             <v-icon>{{ child.icon }}</v-icon>
+           </v-list-tile-action>
+           <v-list-tile-content>
+             <v-list-tile-title>
+               {{ child.text }}
+             </v-list-tile-title>
+           </v-list-tile-content>
+         </v-list-tile>
+       </v-list-group>
+       <v-list-tile v-else @click="" :key="item.text">
+         <v-list-tile-action>
+           <v-icon>{{ item.icon }}</v-icon>
+         </v-list-tile-action>
+         <v-list-tile-content>
+           <v-list-tile-title>
+             {{ item.text }}
+           </v-list-tile-title>
+         </v-list-tile-content>
+       </v-list-tile>
+     </template>
     </v-list>
   </v-navigation-drawer>
-  <v-toolbar color="amber" app absolute clipped-left>
-    <v-toolbar-side-icon @click.native="drawer = !drawer"></v-toolbar-side-icon>
-    <span class="title ml-3 mr-5">Google&nbsp;<span class="text">Keep</span></span>
-    <v-text-field solo-inverted flat label="Search" prepend-icon="search"></v-text-field>
+  <v-toolbar color="blue darken-3" dark app :clipped-left="$vuetify.breakpoint.lgAndUp" fixed>
+    <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <span class="hidden-sm-and-down">Google Contacts</span>
+    </v-toolbar-title>
+    <v-text-field flat solo-inverted prepend-icon="search" label="Search" class="hidden-sm-and-down"></v-text-field>
     <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>apps</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>notifications</v-icon>
+    </v-btn>
+    <div class="text-xs-center">
+      <v-menu offset-y>
+        <v-btn icon color="primary" dark slot="activator">
+          <v-avatar size="32px" tile>
+            <img src="https://vuetifyjs.com/static/doc-images/logo.svg" alt="Vuetify">
+          </v-avatar>
+        </v-btn>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title @click="logOut">Logout</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </div>
   </v-toolbar>
   <v-content>
-    <v-container fluid fill-height class="grey lighten-4">
-      <v-layout justify-center align-center>
-        <v-flex shrink>
-          <v-tooltip right>
-            <v-btn icon large :href="source" target="_blank" slot="activator">
-              <v-icon large>code</v-icon>
-            </v-btn>
-            <span>Source</span>
-          </v-tooltip>
-          <v-tooltip right>
-            <v-btn icon large href="https://codepen.io/johnjleider/pen/jZQNbd" target="_blank" slot="activator">
-              <v-icon large>mdi-codepen</v-icon>
-            </v-btn>
-            <span>Codepen</span>
-          </v-tooltip>
-        </v-flex>
+    <v-container fluid fill-height>
+      <v-layout>
+        <router-view/>
       </v-layout>
     </v-container>
   </v-content>
+  <v-btn fab bottom right color="pink" dark fixed @click.stop="dialog = !dialog">
+    <v-icon>add</v-icon>
+  </v-btn>
+  <v-dialog v-model="dialog" width="800px">
+    <v-card>
+      <v-card-title class="grey lighten-4 py-4 title">
+        Create contact
+      </v-card-title>
+      <v-container grid-list-sm class="pa-4">
+        <v-layout row wrap>
+          <v-flex xs12 align-center justify-space-between>
+            <v-layout align-center>
+              <v-avatar size="40px" class="mr-3">
+                <img src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png" alt="">
+              </v-avatar>
+              <v-text-field placeholder="Name"></v-text-field>
+            </v-layout>
+          </v-flex>
+          <v-flex xs6>
+            <v-text-field prepend-icon="business" placeholder="Company"></v-text-field>
+          </v-flex>
+          <v-flex xs6>
+            <v-text-field placeholder="Job title"></v-text-field>
+          </v-flex>
+          <v-flex xs12>
+            <v-text-field prepend-icon="mail" placeholder="Email"></v-text-field>
+          </v-flex>
+          <v-flex xs12>
+            <v-text-field type="tel" prepend-icon="phone" placeholder="(000) 000 - 0000" mask="phone"></v-text-field>
+          </v-flex>
+          <v-flex xs12>
+            <v-text-field prepend-icon="notes" placeholder="Notes"></v-text-field>
+          </v-flex>
+        </v-layout>
+      </v-container>
+      <v-card-actions>
+        <v-btn flat color="primary">More</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn flat color="primary" @click="dialog = false">Cancel</v-btn>
+        <v-btn flat @click="dialog = false">Save</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
   name: 'l-admin',
   data: () => ({
+    dialog: false,
     drawer: null,
     items: [{
-      icon: 'lightbulb_outline',
-      text: 'Notes'
+      icon: 'contacts',
+      text: 'Contacts'
     },
     {
-      icon: 'touch_app',
-      text: 'Reminders'
+      icon: 'history',
+      text: 'Frequently contacted'
     },
     {
-      divider: true
+      icon: 'content_copy',
+      text: 'Duplicates'
     },
     {
-      heading: 'Labels'
+      icon: 'keyboard_arrow_up',
+      'icon-alt': 'keyboard_arrow_down',
+      text: 'Labels',
+      model: true,
+      children: [{
+        icon: 'add',
+        text: 'Create label'
+      }]
     },
     {
-      icon: 'add',
-      text: 'Create new label'
-    },
-    {
-      divider: true
-    },
-    {
-      icon: 'archive',
-      text: 'Archive'
-    },
-    {
-      icon: 'delete',
-      text: 'Trash'
-    },
-    {
-      divider: true
+      icon: 'keyboard_arrow_up',
+      'icon-alt': 'keyboard_arrow_down',
+      text: 'More',
+      model: false,
+      children: [{
+        text: 'Import'
+      },
+      {
+        text: 'Export'
+      },
+      {
+        text: 'Print'
+      },
+      {
+        text: 'Undo changes'
+      },
+      {
+        text: 'Other contacts'
+      }
+      ]
     },
     {
       icon: 'settings',
@@ -112,7 +199,7 @@ export default {
     },
     {
       icon: 'chat_bubble',
-      text: 'Trash'
+      text: 'Send feedback'
     },
     {
       icon: 'help',
@@ -124,12 +211,17 @@ export default {
     },
     {
       icon: 'keyboard',
-      text: 'Keyboard shortcuts'
+      text: 'Go to the old version'
     }
     ]
   }),
   props: {
     source: String
+  },
+  methods: {
+    logOut () {
+      firebase.auth().signOut()
+    }
   }
 }
 </script>
